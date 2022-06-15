cba3e0f8c050ba9b39087e653198b095e314b4b4
karrakertravis@gmail.com <student@bchd.9f22ce9c-c0c3-4325-b80e-67c6a68fe498>
2022-06-15 21:52:50 +0000

    June 15th 2022 - Comment in code

--- magic8ball.py
+++ magic8ball.py
@@ -10,16 +10,18 @@ answers=["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, defi
 
 def main():
     """Primary Run-time Code"""
+    #A question gets asked by the user.
     os.system('clear')
     question=input("Ask a Yes or No question of the Magic 8 Ball.\n>")
     print("Hmmmm....")
-    time.sleep(random.randrange(2,6))
-    print1by1(random.choice(answers))
+    time.sleep(random.randrange(2,6)) #Force the machine to pause to simulate "thinking".
+    print1by1(random.choice(answers)) #Answer chosen at random and printed one character at a time.
     print()
 
 
 
 def print1by1(text, delay=0.1):
+    """Set parameters for how fast or slow answers get printed"""
     for c in text:
         sys.stdout.write(c)
         sys.stdout.flush()
@@ -29,12 +31,10 @@ def restart():
     """Ask to go again"""
     while True:
         #Ask input to play again
-        #If answer is no, break
-        #If yes, game() gets run again to restart game.
         answer = input("Would you like to ask something else?\n>").capitalize()
-        if answer in ['Yes']:
-            main()
-        elif answer in ['No']:
+        if answer in ['Yes']: 
+            main() #When yes is said, main() gets called on again to restart the program
+        elif answer in ['No']: #Simply put, when no is said, program ends
             print("Goodbye!")
             break
         else:
