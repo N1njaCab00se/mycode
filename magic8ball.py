ce3969769868e7c0a1d4d03b6339fb925438ee41
karrakertravis@gmail.com <student@bchd.9f22ce9c-c0c3-4325-b80e-67c6a68fe498>
2022-06-15 21:45:49 +0000

    June 15th 2022

--- magic8ball.py
+++ magic8ball.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
+""" Generic Magic 8 Ball | Travis Karraker """
 import random
+import time
+import sys
+import os
+
 
 answers=["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, please try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful.", "Nope.", "Trust me, DON'T ask again.", "Sure, why not?"]
 
 def main():
-    question=input("Ask a Yes or No question of the Magic 8 Ball.\n")
-    print(random.choice(answers))
+    """Primary Run-time Code"""
+    os.system('clear')
+    question=input("Ask a Yes or No question of the Magic 8 Ball.\n>")
+    print("Hmmmm....")
+    time.sleep(random.randrange(2,6))
+    print1by1(random.choice(answers))
+    print()
 
 
-main()
+
+def print1by1(text, delay=0.1):
+    for c in text:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(delay)
 
 def restart():
+    """Ask to go again"""
     while True:
         #Ask input to play again
         #If answer is no, break
         #If yes, game() gets run again to restart game.
-        answer = input("Would you like to play again?\n").capitalize()
+        answer = input("Would you like to ask something else?\n>").capitalize()
         if answer in ['Yes']:
             main()
         elif answer in ['No']:
@@ -23,4 +39,8 @@ def restart():
             break
         else:
             print("Please enter either Yes or No.")
+
+main()
 restart()
+
+
