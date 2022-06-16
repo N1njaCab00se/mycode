d9c592729b32d33a23bcfd9e9cdac8b385558904
karrakertravis@gmail.com <student@bchd.9f22ce9c-c0c3-4325-b80e-67c6a68fe498>
2022-06-16 21:28:43 +0000

    June 16th 2022

--- rpg.py
+++ rpg.py
@@ -1,7 +1,89 @@
 #!/usr/bin/python3
-
+from random import randint
+import dice
+import sys
+import os
+import time
 # Replace RPG starter project with this code when new instructions are live
+bestiary = [{'name' : 'goblin', 'health' : 10, 'damage' : '1d5'},
+            {'name' : 'orc', 'health' : 15, 'damage' : '1d8'},
+            {'name' : 'ogre', 'health' : 20, 'damage' : '1d12'}]
+armory = {'knife': {'damage': '1d12'}}
+spell_lookup = {'fireball': {'damage': '4d6'}}
+player_health = 30
+spellbook = []
 
+def combat():
+    monster_ID= randint(0,2)
+
+    global player_health, inventory, armory, bestiary
+    round = 1
+    monster_health = bestiary[monster_ID]['health']   
+    print(f"A ferocious {bestiary[monster_ID]['name']} approaches! COMBAT HAS BEGUN!\n")
+    while True:
+        print(f"ROUND {round}")
+        print("Player Health: [" + str(player_health) + "]")
+        print("Monster Health: [" + str(monster_health) + "]")
+
+        print("Type: RUN or USE [weapon]") # gotta write code for cast
+        move = input().lower().split() # converts move into a lower-case list to deal with each item in list separately
+        monster_damage = sum(dice.roll(bestiary[monster_ID]['damage']))
+        print("\n=========================")
+
+
+        if move[0] == 'use': #
+            if move[1] in inventory: # checks if weapon is in your inventory
+                player_damage = dice.roll(armory[move[1]]['damage'])
+                print(f"You hit a {bestiary[monster_ID]['name']} for {player_damage} damage!")
+            if move[1] not in inventory:
+                print(f"There is no {move[1]} in your inventory!")
+
+        if move[0] == 'cast': #
+            if move[1] in spellbook: # checks if spell is in your spellbook
+                if move[1].lower() == 'fireball':
+                    player_damage = sum(dice.roll(spell_lookup[move[1]]['damage']))
+                    print(f"Summoning eldritch forces, you scorch the {bestiary[monster_ID]['name']} for {player_damage} damage!")
+            if move[1] not in spellbook:
+                print(f"You don't know the {move[1]} spell!")
+
+        if move[0] == 'run': #
+            escape_chance= randint(1,10) #+ player_speed # if I set this variable later, here's where it would work
+
+            if escape_chance >= 10:
+                print("You make a flawless escape!")
+                break
+            if escape_chance >= 5:
+                print("You expose your back as you turn and flee- the monster takes advantage.")
+                print(f"A {bestiary[monster_ID]['name']} hits you for {monster_damage} damage!")
+                player_health -= int(monster_damage)
+                if player_health >= 1:
+                    print("You managed to escape.")
+                    break
+                if player_health < 1:
+                    print("You have been slain.")
+                    print("\nGAME OVER")
+                    sys.exit()
+            if escape_chance >= 0:
+                print("The monster out-maneuvers you and attacks! You do not escape.")
+
+        try:
+            monster_health -= int(player_damage)
+        except:
+            pass
+        if monster_health <= 0:
+            print(f"The {bestiary[monster_ID]['name']} lies dead. You are victorious!\n")
+            del rooms[currentRoom]['item']
+            print("You feel as if there's more to be done before you make a daring escape...")
+            break
+
+        print(f"A {bestiary[monster_ID]['name']} hits you for {monster_damage} damage!")
+        print ("=========================\n")
+        round += 1
+        player_health -= int(monster_damage)
+
+        if player_health <= 0:
+            print("You have been vanquished! You are dead.")
+            sys.exit()
 def showInstructions():
   #print a main menu and the commands
   print('''
@@ -26,6 +108,8 @@ def showStatus():
 
 #an inventory, which is initially empty
 inventory = []
+
+answered = False
 
 # A dictionary linking a room to other rooms
 rooms = {
@@ -125,6 +209,19 @@ while True:
     print('You escaped the house with the ultra rare key and magic potion... YOU WIN!')
     break
 
+# Worship Room Riddle
+  if currentRoom == 'Worship Room' and answered == False:
+      answer=["a dorc", "a dork", "dorc", "dork"]
+      print('As you enter, a statue on the opposite side of the room speaks.')
+      print('\"Your mother is an orc. Your father is a dwarf. What are you?\"')
+      reply=input(">")
+      if reply in answer:
+          print("\"HAH! Your words, not mine. Here, take this since you amused me.\"")
+          answered = True
+      else:
+          print("\"No, that's not it. Now leave this room, only return when you have the answer.\"")
+          currentRoom='Hall'
+
 ## If a player falls through the trapdoor
   if currentRoom == 'Traproom':
       print('You have fallen through a trapdoor! As you get up from the fall, you hear the trapdoor above shut, followed by a heavy clunk. Your fate is sealed. GAME OVER!')
@@ -132,5 +229,4 @@ while True:
 
   ## If a player enters a room with a monster
   elif 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
-    print('A monster has got you... GAME OVER!')
-    break
+    combat()
